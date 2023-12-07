76d495021971c60701b3eb52244c3f2065f3134c
zhao <2644572487@qq.com>
2023-12-07 10:07:39 +0000

    增加关于页

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -37,7 +38,7 @@ export default function Home() {
           height={37}
           priority
         />
-        <span>love jinqiao</span>
+        <Link href={'/about'}>love jinqiao</span>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
